ecebf6dde89d6cc668457329a96a85954c571d8f
zakirangwala <zakirangwala@gmail.com>
2021-01-20 00:13:56 -0500

    firebase config

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
-  appId: process.env.APPID,
-  measurementId: process.env.MEASUREMENTID,
+  apiKey: "AIzaSyBIHpXVyRjvSYKqJsoIPW84K3rE8M6aeks",
+  authDomain: "bridge-b8503.firebaseapp.com",
+  projectId: "bridge-b8503",
+  storageBucket: "bridge-b8503.appspot.com",
+  messagingSenderId: "1079781622960",
+  appId: "1:1079781622960:web:6714afdc232dd401f2a775",
+  measurementId: "G-D0HZG1QBB8"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
