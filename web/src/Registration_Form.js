f222e1c458108380d854197e048775172be4f2f5
zakirangwala <zakirangwala@gmail.com>
2021-01-20 01:36:12 -0500

    validation code

--- web/src/Registration_Form.js
+++ web/src/Registration_Form.js
@@ -1,10 +1,40 @@
 import React, { useState } from "react";
+import firebase from "firebase";
 import { db } from "./firebase";
 
 const Registration_Form = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [number, setNumber] = useState("");
+  const [verification, setVerification] = useState("");
+  const [status, setStatus] = useState("");
+  const [state, setState] = useState("");
+  const [docId, setDocId] = useState("123");
+
+  const verify = () => {
+    if (state === "checking") {
+      return (
+        <div className="field half">
+          <label for="code">*Verification Code</label>
+          <input
+            type="text"
+            name="code"
+            id="code"
+            placeholder="Verification Code"
+            value={verification}
+            onChange={(e) => setVerification(e.target.value)}
+            required
+          />
+        </div>
+      );
+    } else if (state === "Approved") {
+      return <p>Approved</p>;
+    } else if (state === "Error") {
+      return <p>Error</p>;
+    } else {
+      return null;
+    }
+  };
 
   const clearForm = () => {
     setName("");
@@ -13,13 +43,44 @@ const Registration_Form = () => {
   };
 
   const submitForm = (e) => {
-    e.preventDefault();
-    db.collection("users").add({
-      fullname: name,
-      email: email,
-      number: number,
-    });
-    clearForm();
+    if (state === "checking") {
+      setStatus("Verified");
+      e.preventDefault();
+      db.collection("users")
+        .doc(docId)
+        .set({
+          status: status,
+          fullname: name,
+          email: email,
+          number: number,
+          code: verification,
+        })
+        .then(function () {
+          setState("Approved");
+        })
+        .catch(function (error) {
+          setState("Error");
+        });
+    } else {
+      e.preventDefault();
+      setStatus("Not Verified");
+      db.collection("users")
+        .add({
+          status: status,
+          fullname: name,
+          email: email,
+          number: number,
+          code: verification,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        .then(function (docRef) {
+          setDocId(docRef.id);
+          setState("checking");
+        })
+        .catch(function (error) {
+          setState("Error");
+        });
+    }
   };
 
   return (
@@ -32,7 +93,7 @@ const Registration_Form = () => {
               type="text"
               name="name"
               id="name"
-              placeholder="John Doe"
+              placeholder="Full Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
@@ -44,7 +105,7 @@ const Registration_Form = () => {
               type="email"
               name="email"
               id="email"
-              placeholder="johndoe@gmail.com"
+              placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -56,21 +117,18 @@ const Registration_Form = () => {
               type="text"
               name="phone"
               id="phone"
-              placeholder="+19126472134"
+              placeholder="Phone Number"
               value={number}
               onChange={(e) => setNumber(e.target.value)}
               required
             />
           </div>
+          {verify()}
         </div>
         <div className="buttons_react">
           <ul className="actions">
             <li>
-              <input
-                type="submit"
-                value="Send Form"
-                className="primary"
-              />
+              <input type="submit" value="Send Form" className="primary" />
             </li>
             <li>
               <input type="reset" value="Reset" onClick={clearForm} />
